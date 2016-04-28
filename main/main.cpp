90f27d831546a4169951f683ac669822d5605d83
Adexe Sabina <alu0100769609@ull.edu.es>
2016-04-28 23:33:19 +0100

    Guardado hasta el momento para hacer pruebas.. el codigo actual falla

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>         // system()
 #include <stdexcept>       // invalid_argument()
+#include <vector>
 #include "../hpp/DNI.hpp"
 #include "../hpp/Sort.hpp"
 #include <iostream>
@@ -7,11 +8,22 @@
 Adexe Sabina Pérez
 */
 using namespace std;
-typedef int type;
+
+typedef DNI T_KEY;
+#define SIZE 50000000                  // To generate random DNIs
+#define NUMBER_OF_DNI 25               // 25 DNIs
+
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////// DECLARED FUNCIONS //////////////////////////////
+vector<T_KEY> CreateDNI(int);
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// MAIN /////////////////////////////////////
 int main () {
    system("clear");
 
-   DNI miDni[2];
+   vector<T_KEY> myDNIs = CreateDNI(NUMBER_OF_DNI);  // create vector with DNIs
+   Sort<T_KEY> sortDNIs = Sort<T_KEY>(myDNIs,NUMBER_OF_DNI);
 /*
    type v[10] = {15,11,19,16,17,14,12,18,10,13};
 
@@ -28,7 +40,7 @@ int main () {
    t.printItems();
 */
 
-
+/*
    try{
       cout << "Enter DNI-1: ";
       cin >> miDni[0];
@@ -40,6 +52,7 @@ int main () {
       cout << error.what() << endl;
       return 0;
    }
+
    if (miDni[0] >= miDni[1])
       cout << "Is 1 >= than 2?: yes" << endl;
    else
@@ -56,5 +69,18 @@ int main () {
       cout << "Is 1 == 2?: no" << endl;
 
    cout << "DNIs are: " << miDni[0] << ", " << miDni[1] << endl;
+*/
    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////// CREATE_DNI FUNCTION /////////////////////////////
+vector<T_KEY> CreateDNI (int numberOfDNI) {
+   vector<T_KEY> database(numberOfDNI,0); //size, default value
+   T_KEY dni = 0;
+   for (int i = 0; i < numberOfDNI; i++) {
+     dni = rand() % SIZE + 30000001; //random between 30.000.001 & 80.000.000
+     database[i] = dni;
+   };
+   return database;  //Allowed because std::vector is a class
 }
